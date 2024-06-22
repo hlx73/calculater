af32235525ea27d8233496cd8e34dcf8c5bc270b
Hadeel <hadelturki@hotmail.com>
2024-06-22 15:21:13 +0300

    using gril

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,31 @@
     </title>
 </svelte:head>
 
-<div class=" bg-white h-[25rem] w-[18rem] rounded-3xl ">
+<div class=" bg-white h-[20rem] w-[15rem] rounded-3xl grid grid-cols-4 gap-2 ">
+    <button>%</button>
+    <button>√</button>
+    <button> CE</button>
+    <button>C</button>
+
+    <button>7</button>
+    <button>8</button>
+    <button>9</button>
+    <button>-</button>
+
+    <button>4</button>
+    <button>5</button>
+    <button>6</button>
+    <button>÷</button>
+
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+    <button>x</button>
+
+    <button>.</button>
+    <button>0</button>
+    <button>=</button>
+    <button>+</button>
 
 </div>
 
