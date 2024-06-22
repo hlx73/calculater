78e272168dc7ed47dbc3f6211744d54d06e28e67
Hadeel <hadelturki@hotmail.com>
2024-06-22 16:20:01 +0300

    shdows

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,31 +4,37 @@
     </title>
 </svelte:head>
 
-<div class=" bg-white h-[20rem] w-[15rem] rounded-3xl grid grid-cols-4 gap-2 ">
-    <button>%</button>
-    <button>√</button>
-    <button> CE</button>
-    <button>C</button>
+<div class=" bg-white rounded-3xl grid grid-cols-4 gap-1 p-6 font-semibold shadow-xl">
+    <div class="bg-blue-700 rounded-full col-span-4 h-12 flex items-center px-4
+    mb-2 text-white text-lg ">
+        123456
+    </div>
+    <button class="bg-slate-100">%</button>
+    <button class="bg-slate-100">√</button>
+    <button class="bg-slate-100"> CE</button>
+    <button class="bg-blue-950 text-gray-50">C</button>
 
     <button>7</button>
     <button>8</button>
     <button>9</button>
-    <button>-</button>
+    <button class="text-white bg-rose-600">-</button>
 
     <button>4</button>
     <button>5</button>
     <button>6</button>
-    <button>÷</button>
+    <button class="text-white bg-sky-600 ">
+        ÷
+    </button>
 
     <button>1</button>
     <button>2</button>
     <button>3</button>
-    <button>x</button>
+    <button class="bg-yellow-300 text-white ">x</button>
 
     <button>.</button>
     <button>0</button>
     <button>=</button>
-    <button>+</button>
+    <button class="text-white bg-green-400">+</button>
 
 </div>
 
